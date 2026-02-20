1cee04de37379200c881be5d4bd9168ff0b414da
semnil5202 <semnil5202@gmail.com>
2026-02-21 00:08:02 +0900

    chore: common theme

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 
+import './globals.css';
+
 export const metadata: Metadata = {
   title: 'seeun-log admin',
   description: 'seeun-log 관리자 페이지',
