d87c52b34512629de9354a824539f598edf472e1
semnil5202 <semnil5202@gmail.com>
2026-03-03 08:38:44 +0900

    refactor: use www subdomain

--- packages/config/site.ts
+++ packages/config/site.ts
@@ -1,12 +1,12 @@
 export const SITE_NAME_KO = '은민로그';
 export const SITE_NAME_EN = 'eunminlog';
-export const SITE_URL = 'https://eunminlog.site';
+export const SITE_URL = 'https://www.eunminlog.site';
 export const GA_MEASUREMENT_ID = 'G-QX8XPFX6YK';
 
 type Stage = 'production' | 'development';
 
 const SITE_URL_MAP: Record<Stage, string> = {
-  production: 'https://eunminlog.site',
+  production: 'https://www.eunminlog.site',
   development: 'https://dev.eunminlog.site',
 };
 
